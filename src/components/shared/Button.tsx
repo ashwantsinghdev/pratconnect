defb85f2ced387b712c25aab845dce76bea1a997
Ashwant Singh <ashwantconset@gmail.com>
2026-06-24 12:11:15 +0530

    button shared component created

---
+++ src/components/shared/Button.tsx
@@ -0,0 +1,58 @@
+import type { FC } from "react";
+import "remixicon/fonts/remixicon.css";
+
+const ButtonModel = {
+  primary:
+    "bg-blue-500 hover:bg-blue-600 rounded font-medium text-white px-6 py-2",
+  secondary:
+    "bg-indigo-500 hover:bg-indigo-600 rounded font-medium text-white px-6 py-2",
+  danger:
+    "bg-rose-500 hover:bg-rose-600 rounded font-medium text-white px-6 py-2",
+  warning:
+    "bg-amber-500 hover:bg-amber-600 rounded font-medium text-white px-6 py-2",
+  dark: "bg-zinc-500 hover:bg-zinc-600 rounded font-medium text-white px-6 py-2",
+  success:
+    "bg-green-400 hover:bg-green-500 rounded font-medium text-white px-6 py-2",
+  info: "bg-cyan-500 hover:bg-cyan-600 rounded font-medium text-white px-6 py-2",
+};
+
+interface ButtonInterFace {
+  children?: string;
+  type?:
+    | "primary"
+    | "secondary"
+    | "danger"
+    | "warning"
+    | "dark"
+    | "success"
+    | "info";
+  onClick?: () => void;
+  icon?: string;
+  key?: string | number;
+  loading?: boolean;
+}
+
+const Button: FC<ButtonInterFace> = ({
+  children = "Submit",
+  key = 0,
+  type = "primary",
+  onClick,
+  icon,
+  loading = false,
+}) => {
+  if (loading)
+    return (
+      <button disabled className="text-gray-400">
+        <i className="fa fa-spinner fa-spin mr-2">proccessing..</i>
+      </button>
+    );
+
+  return (
+    <button className={ButtonModel[type]} onClick={onClick} key={key}>
+      {icon && <i className={`ri-${icon} mr-1`}></i>}
+      {children}
+    </button>
+  );
+};
+
+export default Button;
