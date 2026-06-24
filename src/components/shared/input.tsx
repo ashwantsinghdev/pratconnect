5e9f60dd90b329595f099d61f5b92e96562349a7
Ashwant Singh <ashwantconset@gmail.com>
2026-06-24 11:25:42 +0530

    input component rewrite

--- src/components/shared/input.tsx
+++ src/components/shared/input.tsx
@@ -1,27 +1,23 @@
 import type { FC } from "react";
 
-interface InputInterface {
-  name: string;
-  type: string;
-  placeholder?: string;
-  key?: string | number;
+interface InputInterFace {
+  name:string
+  type?:string
+  placeholder?:string
+  key?:string | number
+
 }
 
-const input: FC<InputInterface> = ({
-  key = 0,
-  name,
-  placeholder,
-  type = "text",
-}) => {
-  return (
+const Input:FC<InputInterFace> =({name,type="text",placeholder,key=0})=>{
+  return(
     <input
-      name={name}
-      type={type}
-      className=""
-      placeholder={placeholder}
-      key={key}
+    name={name}
+    type={type}
+    className="border border-gray-300 rounded px-3 py-2 w-full"
+    placeholder={placeholder}
+    key={key}
     />
-  );
-};
+  )
+}
 
-export default input;
+export default Input
