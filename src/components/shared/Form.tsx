29fb952cbbf801bc32667cf0f080a5eb2076d723
Ashwant Singh <ashwantconset@gmail.com>
2026-06-24 17:24:49 +0530

    created form shared component handler

---
+++ src/components/shared/Form.tsx
@@ -0,0 +1,43 @@
+import type { FC, ReactNode, FormEvent } from "react";
+
+export type FormDataType = Record<string, string>;
+
+interface FormInterface {
+  children: ReactNode;
+  className: string;
+  reset?: boolean;
+  onValue: (value: FormDataType) => void;
+}
+
+const Form: FC<FormInterface> = ({
+  children,
+  className,
+  reset = false,
+  onValue,
+}) => {
+  const handleForm = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const form = e.currentTarget;
+    const formData = new FormData(form);
+    const data: FormDataType = {};
+
+    formData.forEach((value, name) => {
+      data[name] = value.toString();
+    });
+
+    if (onValue) {
+      onValue(data);
+      if (reset) {
+        form.reset();
+      }
+    }
+  };
+
+  return (
+    <form className={className} onSubmit={handleForm}>
+      {children}
+    </form>
+  );
+};
+
+export default Form;
