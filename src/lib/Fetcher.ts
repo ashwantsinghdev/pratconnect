d1bf1d49eac3b7fcb6a7fc591221e9af82a4545f
Ashwant Singh <ashwantconset@gmail.com>
2026-06-29 13:30:21 +0530

    added catch fetcher and socket in lib

---
+++ src/lib/Fetcher.ts
@@ -0,0 +1,12 @@
+import HttpInterceptor from "./HttpInterceptor";
+
+const Fetcher = async (url: string) => {
+  try {
+    const { data } = await HttpInterceptor.get(url);
+    return data;
+  } catch (err: unknown) {
+    throw new Error("Request failed", { cause: err });
+  }
+};
+
+export default Fetcher;
